ebb10d3f7668a46088e00e2998ea73c57b96dc93
Alvyn Abranches <alvynabranches@gmail.com>
2023-06-07 15:34:29 +0530

    Update docker-clean.sh

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,4 +1,4 @@
 docker system prune
-docker contianer rm -f $(docker container ls -aq)
+docker container rm -f $(docker container ls -aq)
 docker image rm -f $(docker image ls -aq)
 docker volume rm -f $(docker volume ls -q)
